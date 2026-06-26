bb683cbd8b09c0ed03318aa76ce2db4374f10e38
GitHub ashwxie <GitHub ashwxie@gmail.com>
2026-06-26 08:56:39 +0800

    fix: clean code

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,12 +2,10 @@
 import { useState } from "react";
 import { login } from "@/features/auth/services/auth.service";
 import { useAuthStore } from "@/store/auth.store";
-import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
   const [loading, setLoading] = useState(false);
   const setAuth = useAuthStore((s) => s.setAuth);
-  const router = useRouter();
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -29,6 +27,7 @@ export default function LoginPage() {
         <button className="bg-blue-600 text-white px-4 py-2 rounded">
           {loading ? "Authenticating..." : "Login to Dashboard"}
         </button>
+
       </form>
     </div>
   );
